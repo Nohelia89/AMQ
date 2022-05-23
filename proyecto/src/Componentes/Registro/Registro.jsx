346e306ad6526d22171dae199d5d93bb829f8e3f
Nohelia Yanibelli <nyanibelli89@gmail.com>
2022-05-23 19:32:43 -0300

    registro sin terminar

--- proyecto/src/Componentes/Registro/Registro.jsx
+++ proyecto/src/Componentes/Registro/Registro.jsx
@@ -1,26 +1,72 @@
 
 import { Form } from 'react-bootstrap';
 import './Registro.css';
-
+import React, { useState } from 'react'
 
 
 
 
 export default function Registro() {
+
+
+  const [tipoUsuario, inputType] = useState('huesped');
+
+
+  const setInputToHuesped = () => {
+
+  inputType('huesped')
+  }
+
+  const setInputToHost= () => {
+
+   inputType('anfitrion')
+  }
     return (
+
+<>
 <div class="bod">
       <form class="form1">
       <div class="tit">REGISTRO</div>
-      <input type="checkbox" id="cbox1" value="pricheck"/> <label for="cbox2">Huesped</label> 
-      <input type="checkbox" id="cbox2" value="segcheck"/> <label for="cbox2">Anfitrion</label>
-      <div class="input_container">
+      <input type="checkbox" id="cbox1" value="pricheck"/> onClick={setInputToHuesped()}<label for="cbox2">Huesped</label> 
+      <input type="checkbox" id="cbox2" value="segcheck"/> onClick={setInputToHost()} <label for="cbox2">Anfitrion</label>
+
+      {
+        tipoUsuario === 'huesped' ?
+
+          <>
+
+<div class="input_container">
       <p class="form-input2" type="Nombre:"><input class="form-input1" ></input></p>
       <p class="form-input2" type="Apellido:"><input class="form-input1" ></input></p></div>
       <p class="form-input2" type="Mail:"><input class="form-input1" ></input></p>
-      <button class = "btn submits boton">Registrar</button>
+
+      <button class = "btn submits boton">Registrar huesped</button>
      
-    </form>
+
+          </>
+
+          :
+
+                <>
+
+<div class="input_container">
+      <p class="form-input2" type="Nombre:"><input class="form-input1" ></input></p>
+      <p class="form-input2" type="Apellido:"><input class="form-input1" ></input></p></div>
+      <p class="form-input2" type="Mail:"><input class="form-input1" ></input></p>
+
+      <button class = "btn submits boton">Registrar anfitrion</button>
+     
+     
+
+         
+
+          </>
+            
+      }
+ </form>
+
+    
   </div>
-      
+  </>
  )
 } 
