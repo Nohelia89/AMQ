0159751997a3a69b8fac3fb7a153947766ab5861
jgon7 <jessica.gonzalez0710@gmail.com>
2022-05-23 00:09:48 -0300

    alta alojamiento

--- proyecto/src/Componentes/Alojamiento/firebase.js
+++ proyecto/src/Componentes/Alojamiento/firebase.js
@@ -23,6 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const getFirestoreApp = () => {
-  return app
+export function getFirestoreApp() {
+  return app;
 }
+
--- proyecto/src/Componentes/Main/Main.jsx
+++ proyecto/src/Componentes/Main/Main.jsx
@@ -1,10 +1,15 @@
+import AltaAlojamiento from "../Alojamiento/AltaAlojamiento";
 import UserList from "../UserList/UserList";
 
-export default function Main() {
+export default function Main
+() {
     return (
 
+    <>
+      
+      <AltaAlojamiento />
 
-      <UserList/>
+    </>
       
  )
 } 
