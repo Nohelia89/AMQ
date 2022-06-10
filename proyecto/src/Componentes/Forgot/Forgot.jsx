10b6e943b4326d101fcda4292f8de13048612c20
JoNohelia Yanibelli <nyanibelli89@gmail.com>
2022-06-10 09:25:05 -0300

    forgot terminado del front

--- proyecto/src/Componentes/Forgot/Forgot.jsx
+++ proyecto/src/Componentes/Forgot/Forgot.jsx
@@ -5,32 +5,47 @@ import './Forgot.css';
 export default function Forgot() {
 
 
-  const [email, setEmail] = useState('')
-  const Resetear = (email) => {
+
+  const [dataForm, setDataForm] = useState({ email:''})
+
+
+
+  const Resetear = async (e) => {
+    e.preventDefault();
+  var sendMail = {
+
+    email: dataForm.email
+
+  } ;
+
+console.log(sendMail+ "SOY SENDMAIL")    
+    axios.post(`http://localhost:8080/usuario/resetPassword` , sendMail
     
-    axios.post(`http://localhost:8080/usuario/resetPassword` , email,{
-      headers: 
-      { "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": 
-      "Origin, X-Requested-With, Content-Type, Accept"
-      }
-    }  )
+      )
                 
                 .then(res => {
                   alert("Se envió email")
                   console.log(res.data)
                 })
     }
+
+
+    const handleChange = (e) => {
+      setDataForm({
+    
+        [e.target.name]: e.target.value
+      })
+    }
     return (
 <div class="bod">
-      <form class="form3">
+      <form class="form3" onSubmit={Resetear}>
       <div class="tit2"><h2>¿Olvidaste tu contraseña?</h2>Si tu correo es correcto se enviarán los datos para reestablecerla
     
       </div>
 
-      <p class="form-input2" type="Email" ><input class="form-input1" onChange={(event) => setEmail(event.target.value)} placeholder="Ingrese su mail aquí"></input></p>
+      <p class="form-input2" type="Mail" ><input required class="form-input1" name='email' type='email' value={dataForm.email} onChange={handleChange} placeholder='Ingrese email'></input></p>
     
-      <button class = "btn submits boton" onClick={() => Resetear(email)}>Enviar</button>
+      <button class = "btn submits boton">Enviar</button>
      
     </form>
   </div>
